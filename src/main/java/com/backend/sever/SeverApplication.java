dbfe211a375d60bbea9134500e879a550bcf274c
qogustj <haulqogustj@naver.com>
2023-04-07 15:45:34 +0900

    dd

--- src/main/java/com/backend/sever/SeverApplication.java
+++ src/main/java/com/backend/sever/SeverApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeverApplication {
-//
+//r
 	public static void main(String[] args) {
 		SpringApplication.run(SeverApplication.class, args);
 	}
